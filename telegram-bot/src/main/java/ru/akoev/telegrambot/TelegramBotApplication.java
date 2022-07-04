b3d7774e8ebe8a32ab0ba53746a5ee8b2a39642e
Gennadiy Akoev <akoev_2000_gena@mail.ru>
2022-07-04 20:38:45 +0300

    Выполнение практического задания 1. Реализовал начальную страницу.

---
+++ telegram-bot/src/main/java/ru/akoev/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,15 @@
+package ru.akoev.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.*;
+import java.net.URL;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+
+}
