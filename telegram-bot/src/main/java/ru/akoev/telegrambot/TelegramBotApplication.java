4ce446cc00ee50837d9d17d0b2a3d4240c7b195f
Gennadiy Akoev <akoev_2000_gena@mail.ru>
2022-07-05 10:50:29 +0300

    Выполнение практического задания 1. Произведены изменения в проекте. Исправлена версия SDK с 18 на 11. Реализовал автоматическое открытие начальной страницы в браузере при помощи библиотеки AWT.

--- telegram-bot/src/main/java/ru/akoev/telegrambot/TelegramBotApplication.java
+++ telegram-bot/src/main/java/ru/akoev/telegrambot/TelegramBotApplication.java
@@ -3,13 +3,24 @@ package ru.akoev.telegrambot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(TelegramBotApplication.class, args);
+
+		String url = "http://localhost:8080/";
+		try {
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
